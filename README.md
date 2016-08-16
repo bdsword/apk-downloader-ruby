6885dc05e820bc2006ee87059c5a9e852e119040
BDSword <thetruebloodsword@gmail.com>
2016-08-16 20:08:12 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# apk-downloader
+
+This script can help you download top selling apps from Google Play Store.
+
+Using ruby gem market_bot, we can gather more information than the python approach of the master branch.
+
+# Requirements
+
+- [protobuf](https://github.com/google/protobuf)
+
+Refer [egirault/googleplay-api](https://github.com/egirault/googleplay-api) for more details.
+
+Briefly,
+
+```bash
+git clone https://github.com/google/protobuf
+cd protobuf
+./autogen.sh
+./configure
+make
+sudo make install
+cd python
+python setup.py build
+sudo python setup.py install
+```
+
+# Init this repo
+
+```bash
+git clone git@github.com:bdsword/apk-downloader.git
+git submodule init
+git submodule update
+```
+
+Then, setup your google account and password/auth-token in **googleplay_api/config.py**.
+
+# Usage
+
+```bash
+ruby download.rb
+```
---
+++ download.rb
@@ -0,0 +1,49 @@
+require 'market_bot'
+require 'sqlite3'
+require 'securerandom'
+require 'time'
+
+File.delete('report.sqlite3') if File.exist?('report.sqlite3')
+
+db = SQLite3::Database.new 'report.sqlite3'
+
+rows = db.execute <<-SQL
+    create table apps (
+        app_id text, version text, category text, num_download text, rating float,
+        app_size integer, file_path varchar(512), rank integer, update_time datetime, access_time datetime default (datetime(current_timestamp, 'LOCALTIME')),
+        primary key (app_id, update_time)
+    );
+SQL
+
+ignore_categories = ['ANDROID_WEAR']
+app_num = 0
+
+MarketBot::Play::Chart::CATEGORIES.each do |category|
+    unless ignore_categories.include?(category)
+        puts "Category #{category}"
+        chart = MarketBot::Play::Chart.new('topselling_free', category, country: 'us', lang: 'en_US')
+        chart.update
+        chart.result.each do |x|
+            app = MarketBot::Play::App.new(x[:package])
+            app.update
+
+            file_name = SecureRandom.uuid
+            file_path = "./download/#{file_name}.apk"
+            result = `python2 -W ignore ./googleplay_api/download.py #{x[:package]} #{file_path}`
+            if /^Downloading \d+.\d+(KB|MB|GB)... Done\n$/.match(result).nil?
+                puts "Download #{x[:package]} failed..."
+                next
+            end
+
+            puts db.execute("INSERT INTO apps (app_id, version, category, num_download, rating, update_time, app_size, file_path, rank)
+                             VALUES (?,?,?,?,?,?,?,?,?)", [x[:package], app.current_version, app.category, app.installs, app.rating,
+                             Time.parse(app.updated).strftime('%Y-%m-%d %H:%M:%S'), File.size(file_path), file_name, x[:rank]])
+            app_num = app_num + 1
+            puts "##{app_num} finish"
+            sleep(5)
+        end
+        sleep(5)
+    end
+end
+
+
